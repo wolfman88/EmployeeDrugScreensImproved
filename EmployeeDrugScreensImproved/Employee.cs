39835b7a055be70bdd5a772feddd42ba386ed5c4
jth213 <jhopkins213@gmail.com>
2020-09-26 21:39:16 -0400

    Class Instantiation

--- EmployeeDrugScreensImproved/Employee.cs
+++ EmployeeDrugScreensImproved/Employee.cs
@@ -13,26 +13,26 @@ namespace EmployeeDrugScreensImproved
 
         }
 
-        public string ID { get; set; }
-        public string NamePrefix { get; set; }
-        public string FirstName { get; set; }
-        public string MiddleInitial { get; set; }
-        public string LastName { get; set; }
-        public string Gender { get; set; }
-        public DateTime DrugTestDateLast { get; set; }
-        public string EMail { get; set; }
-        public DateTime DateOfBirth { get; set; }
-        public DateTime DateHired { get; set; }
-        public int Salary { get; set; }
-        public int LastPayHike { get; set; }
-        public string SSN { get; set; }
-        public string PhoneNumber { get; set; }
-        public string County { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public string ZipCode { get; set; }
-        public string UserName { get; set; }
-        public string Password { get; set; }
+        public string ID { get; set; } // 0
+        public string NamePrefix { get; set; } // 1
+        public string FirstName { get; set; } // 2
+        public string MiddleInitial { get; set; } // 3
+        public string LastName { get; set; } // 4
+        public string Gender { get; set; } // 5
+        public DateTime DrugTestDateLast { get; set; } // 6
+        public string EMail { get; set; } // 7
+        public DateTime DateOfBirth { get; set; } // 8
+        public DateTime DateHired { get; set; } // 9
+        public string Salary { get; set; } // 10
+        public string LastPayHike { get; set; } // 11
+        public string SSN { get; set; } // 12
+        public string PhoneNumber { get; set; } // 13
+        public string County { get; set; } // 14
+        public string City { get; set; } // 15 
+        public string State { get; set; } // 16
+        public string ZipCode { get; set; } // 17
+        public string UserName { get; set; } // 18
+        public string Password { get; set; } // 19
 
         public Employee GetEmployee(string ID)
         {
