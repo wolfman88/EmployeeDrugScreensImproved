8c34c25d984ae81d41dcd6b8a29a6f67eaac6c7b
Robert Baskin <rbaskin@gmail.com>
2020-09-27 09:49:14 -0400

    creation of First Dictionary

--- EmployeeDrugScreensImproved/Program.cs
+++ EmployeeDrugScreensImproved/Program.cs
@@ -7,57 +7,57 @@ using System.Threading.Tasks;
 
 namespace EmployeeDrugScreensImproved
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      string line = "";
-
-      StreamReader reader = new StreamReader(@"EmployeeRecords.csv");
-      Dictionary<string, Employee> employeeInfoData = new Dictionary<string, Employee>();
-      //Dictionary<int, DateTime> employeesToTest = new Dictionary<int, DateTime>();
-      //Dictionary<int, DateTime> employeesSelectedForTesting = new Dictionary<int, DateTime>();
-
-      int i = 1;
-
-      while (line != null)
-      {
-
-        line = reader.ReadLine();
-
-        if (line != null)
+        static void Main(string[] args)
         {
-          if (i > 1)
-          {
-            string[] employeeInformation = line.Split(',');
-            // stuck here
-            Employee emp = new Employee();
-            emp.ID = employeeInformation[0];
-            emp.NamePrefix = employeeInformation[1];
-            emp.FirstName = employeeInformation[2];
-            emp.MiddleInitial = employeeInformation[3];
-            emp.LastName = employeeInformation[4];
-            emp.Gender = employeeInformation[5];
-            emp.DrugTestDateLast = Convert.ToDateTime(employeeInformation[6]);
-            emp.EMail = employeeInformation[7];
-            emp.DateOfBirth = Convert.ToDateTime(employeeInformation[8]);
-            emp.DateHired = Convert.ToDateTime(employeeInformation[9]);
-            emp.Salary = employeeInformation[10];
-            emp.LastPayHike = employeeInformation[11];
-            emp.SSN = employeeInformation[12];
-            emp.PhoneNumber = employeeInformation[13];
-            emp.County = employeeInformation[14];
-            emp.City = employeeInformation[15];
-            emp.State = employeeInformation[16];
-            emp.ZipCode = employeeInformation[17];
-            emp.UserName = employeeInformation[18];
-            emp.Password = employeeInformation[19];
+            string line = "";
 
-            employeeInfoData.Add(employeeInformation[0], emp);
-          }
-          i++;
+            StreamReader reader = new StreamReader(@"EmployeeRecords.csv");
+            Dictionary<string, Employee> employeeInfoData = new Dictionary<string, Employee>();
+            //Dictionary<int, DateTime> employeesToTest = new Dictionary<int, DateTime>();
+            //Dictionary<int, DateTime> employeesSelectedForTesting = new Dictionary<int, DateTime>();
+
+            int i = 1;
+
+            while (line != null)
+            {
+                line = reader.ReadLine();
+
+                if (line != null)
+                {
+                    if (i > 1)
+                    {
+                        string[] employeeInformation = line.Split(',');
+                        // got stuck here
+                        Employee emp = new Employee();
+                        emp.ID = employeeInformation[0];
+                        emp.NamePrefix = employeeInformation[1];
+                        emp.FirstName = employeeInformation[2];
+                        emp.MiddleInitial = employeeInformation[3];
+                        emp.LastName = employeeInformation[4];
+                        emp.Gender = employeeInformation[5];
+                        emp.DrugTestDateLast = Convert.ToDateTime(employeeInformation[6]);
+                        emp.EMail = employeeInformation[7];
+                        emp.DateOfBirth = Convert.ToDateTime(employeeInformation[8]);
+                        emp.DateHired = Convert.ToDateTime(employeeInformation[9]);
+                        emp.Salary = employeeInformation[10];
+                        emp.LastPayHike = employeeInformation[11];
+                        emp.SSN = employeeInformation[12];
+                        emp.PhoneNumber = employeeInformation[13];
+                        emp.County = employeeInformation[14];
+                        emp.City = employeeInformation[15];
+                        emp.State = employeeInformation[16];
+                        emp.ZipCode = employeeInformation[17];
+                        emp.UserName = employeeInformation[18];
+                        emp.Password = employeeInformation[19];
+
+                        employeeInfoData.Add(employeeInformation[0], emp);
+                    }
+                    i++;
+                }
+            }
+            Console.ReadLine();
         }
-      }
     }
-  }
 }
